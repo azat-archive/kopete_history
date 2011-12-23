79cc899da963930b63cec2906432e27fb9ec37a8
Azat Khuzhin <a3at.mail@gmail.com>
2011-12-23 15:17:09 +0400

    Write full test

--- tests/src/testmerger.cpp
+++ tests/src/testmerger.cpp
@@ -9,9 +9,11 @@
  */
 
 #include <QtTest/QtTest>
+#include <QProcess>
 
 #include "../../src/merger.h"
 
+#define TCALL(callback) try { callback; } catch (const MergerException& e) { QFAIL(e.getMessage().toStdString().c_str()); }
 
 // interface
 class TestMerger : public QObject {
@@ -29,30 +31,98 @@ private slots:
 
 private:
 	Merger* instance;
+	QDir* dataDirectory;
+
+	bool checkDirectoryRecursive(const QDir src, const QDir dst);
+	void removeDirectoryRecursive(const QDir what);
+};
+
+class TestMergerException : public MergerException {
+public:
+	TestMergerException(QString message) : MergerException(message) {}
 };
 
 
 // implementation
 TestMerger::TestMerger() {
 	instance = new Merger();
+
+	QDir relativeDataDirectory("data");
+	dataDirectory = new QDir(relativeDataDirectory.absolutePath());
 }
 
 TestMerger::~TestMerger() {
 	delete instance;
+	delete dataDirectory;
 }
 
-void TestMerger::initTestCase() {}
+void TestMerger::initTestCase() {
+	QDir out = dataDirectory->absolutePath() + "/out";
 
-void TestMerger::cleanupTestCase() {}
+	if (!out.exists()) {
+		out.mkdir(out.absolutePath());
+	} else {
+		TCALL(removeDirectoryRecursive(out));
+	}
+}
+
+void TestMerger::cleanupTestCase() {
+	QDir out = dataDirectory->absolutePath() + "/out";
+
+	TCALL(removeDirectoryRecursive(out));
+}
 
 void TestMerger::testRun() {
-	QDir data("tests/data");
+	int messages = 0;
 
-	instance->addPath(data.path() + "/first/")
-		  ->addPath(data.path() + "/second/")
-		  ->run(data.path() + "/out/");
+	TCALL(
+		messages = instance->addPath(dataDirectory->path() + "/first/")
+						->addPath(dataDirectory->path() + "/second/")
+						->run(dataDirectory->path() + "/out/")
+	);
 
-	// @TODO - add check
+	QVERIFY(messages > 0);
+
+	bool status = false;
+	TCALL(status = checkDirectoryRecursive(dataDirectory->path() + "/out_dist/", dataDirectory->path() + "/out/"));
+	QVERIFY(status == true);
+}
+
+bool TestMerger::checkDirectoryRecursive(const QDir src, const QDir dst) {
+	QProcess *diff = new QProcess(this);
+
+	QStringList arguments;
+	arguments << "-r" << src.path() << dst.path();
+
+	diff->start("diff", arguments);
+
+	if (!diff->waitForFinished()) {
+		throw TestMergerException("Failed to execute diff");
+	}
+
+	return diff->exitCode() == 0;
+}
+
+void TestMerger::removeDirectoryRecursive(const QDir what) {
+	foreach (QString srcFile, what.entryList()) {
+		if (srcFile == "." || srcFile == "..") {
+			continue;
+		}
+
+		QFileInfo srcPath(what.path() + QDir::separator() + srcFile);
+
+		// recursive
+		if (srcPath.isDir()) {
+			removeDirectoryRecursive(srcPath.filePath());
+
+			QDir dir(srcPath.filePath());
+			dir.rmdir(srcPath.filePath());
+		} else {
+			if (!QFile::remove(srcPath.filePath())) {
+				throw TestMergerException("Failed to remove '" + srcPath.filePath() + "'");
+			}
+		}
+	}
 }
 
 
